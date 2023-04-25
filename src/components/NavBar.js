edb1134eed5956e8a6131992ca84d2aca9ac5a96
Praveekorn Chianglong <mtl91475@murata.com>
2023-04-25 16:52:55 +0700

    Banner

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import logo from '../assets/img/logo.svg'
-import navIcon1 from '../assets/img/nav-icon1.svg'
-import navIcon2 from '../assets/img/nav-icon2.svg'
-import navIcon3 from '../assets/img/nav-icon3.svg'
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+// import { HashLink } from "react-router-hash-link";
 
 const NavBar = () => {
   const [activeLink, setActiveLink] = useState("home");
@@ -24,9 +24,9 @@ const NavBar = () => {
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
-  const onUpdateActiveLink = (value)=>{
+  const onUpdateActiveLink = (value) => {
     setActiveLink(value);
-  }
+  };
 
   return (
     <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
@@ -34,11 +34,11 @@ const NavBar = () => {
         <Navbar.Brand href="#home">
           <img src={logo} alt="LOGO" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <span className="navbar-toggler-icon"></span>
-        <Navbar.Toggle />
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link
               href="#home"
               className={
@@ -69,13 +69,21 @@ const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt=""/></a>
-                <a href="#"><img src={navIcon2} alt=""/></a>
-                <a href="#"><img src={navIcon3} alt=""/></a>
+              <a href="#">
+                <img src={navIcon1} alt="" />
+              </a>
+              <a href="#">
+                <img src={navIcon2} alt="" />
+              </a>
+              <a href="#">
+                <img src={navIcon3} alt="" />
+              </a>
             </div>
-            <button className="vvd" onClick={() => console.log("connect")}>
-              <span>Let's Connect</span>
-            </button>
+            {/* <HashLink to="#connect"> */}
+              <button className="vvd">
+                <span>Let’s Connect</span>
+              </button>
+            {/* </HashLink> */}
           </span>
         </Navbar.Collapse>
       </Container>
